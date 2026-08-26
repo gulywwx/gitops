59649cf05616d6018e02d89616c5e1db0f6b1551
Xuefeng <gulyxxf@gmail.com>
2026-08-26 12:10:28 +1000

    cicd bugfix

--- infra/envs/qa/main.tf
+++ infra/envs/qa/main.tf
@@ -25,7 +25,7 @@ module "eks" {
   env                = local.env
   vpc_id             = module.vpc.vpc_id
   subnet_ids         = module.vpc.private_subnets
-  kubernetes_version = "1.33"
+  kubernetes_version = "1.35"
   instance_types     = ["t3.medium"]
   min_size           = 1
   max_size           = 3
